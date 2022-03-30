05c6f8035ecbb18fac7901d55b025a6b68811a22
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-03-30 21:40:33 +0000

    update thinking out loud section

--- leetcode-inJS/mostCommonWord819/819.js
+++ leetcode-inJS/mostCommonWord819/819.js
@@ -11,4 +11,16 @@ Question:
 Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
 
 The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+
+Thinking out loud:
+banned a list of a string array of banned words
+paragraph is a string
+
+return most frequently used word that is not banned
+--> ans is unique as we are guaranteed that @ least one word isn't banned
+        "paragraph" (a string) of banned words in "banned"
+--> words in "paragraph" are case-insensitive 
+        so returned ans in lowercase
+*about string manipulation
+JS implementation string data type
 */
