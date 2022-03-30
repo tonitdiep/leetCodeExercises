92b1b71082219f76553ee6f9883cb66f43bd5318
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-03-30 21:33:15 +0000

    Question

---
+++ leetcode-inJS/mostCommonWord819/819.js
@@ -0,0 +1,14 @@
+
+
+var mostCommonWord = function(paragraph, banned) {}
+
+
+/*
+Input: paragraph = "Bob hit a ball, the hit BALL flew far after it was hit.", banned = ["hit"]
+Output: "ball"
+
+Question:
+Given a string paragraph and a string array of the banned words banned, return the most frequent word that is not banned. It is guaranteed there is at least one word that is not banned, and that the answer is unique.
+
+The words in paragraph are case-insensitive and the answer should be returned in lowercase.
+*/
