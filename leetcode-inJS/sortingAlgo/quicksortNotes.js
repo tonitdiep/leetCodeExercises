c95884ab72f70998cebe782328e05f7deff86c6d
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-06-24 20:15:37 +0000

    update: learning quicksort

--- leetcode-inJS/sortingAlgo/quicksortNotes.js
+++ leetcode-inJS/sortingAlgo/quicksortNotes.js
@@ -1,4 +1,8 @@
 
-//learning quick sort algorithms
-  //establish a pivot point ina   collection of elments
+//learning quick sort algorithms + what is happening:
   //divide and conquer approach
+  //establish a pivot point ina   collection of elments
+  //now use the pivot point as a partition so that smaller elements are moved to the Left of the pivot point WHILE the rest of the elements greater than the pivot point are placed on the Right of it
+//this breaking up of smaller and larger elements based on a pivot point continues to break down from the given unsorted collection into a single-element lists 
+//once we finish quick sorting from the pivot point we can then return the collection of elements in an entire sorted list 
+//there are many ways to choose a pivot point, as what is described from above is just one way.
